76ff4240630d43ea48f2777619eb280af0f9cbc2
Nguyen Hung Cuong <hungcuongqn86@gmail.com>
2017-05-19 14:13:03 +0700

    set layout

--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -14,37 +14,19 @@ export class DesignComponent implements OnInit {
     arrBaseTypes: any = [];
     arrBase: any = [];
     fDesign: any = JSON.parse('{"sBaseType":""}');
-    oDesign: any = JSON.parse('{"sId":"","sFace":"front","sColor":"","sImageFront":"","sImageBack":""}');
+    oDesign: any = JSON.parse('{"sId":"","sFace":"front","sImageFront":"","sImageBack":""}');
     draw: any;
+    productColor: any;
+    productImg: any;
 
     constructor(private DesignService: DesignService) {
     }
 
     ngOnInit() {
         this.getBaseTypes();
-
-        this.draw = SVG('drawing').size(500, 600);
-        const image = this.draw.image('https://cdn.img42.com/4b6f5e63ac50c95fe147052d8a4db676.jpeg');
-        image.size(100, 100);
-        image.opacity(0.5);
-        image.click(function () {
-            this.selectize();
-            image.resize();
-            image.draggable({
-                minX: 0
-                , minY: 0
-                , maxX: 500
-                , maxY: 530
-            });
-        });
-
-        this.draw.click(function() {
-            // image.selectize(false);
-        });
-
-        // const box = draw.viewbox();
-        // const box = draw.viewbox({ x: 0, y: 0, width: 297, height: 210 });
-        // const zoom = box.zoom;
+        this.draw = SVG('drawing').size(530, 630);
+        this.productColor = this.draw.rect(530, 630).fill('#ffffff');
+        this.productImg = this.draw.image();
     }
 
     private getBaseTypes() {
@@ -76,8 +58,8 @@ export class DesignComponent implements OnInit {
                 this.arrBase = data;
                 if (this.arrBase.length > 0) {
                     // if (this.oDesign.sId === '') {
-                        const baseid = this.arrBase[0].id;
-                        this.selectBase(baseid);
+                    const baseid = this.arrBase[0].id;
+                    this.selectBase(baseid);
                     // }
                 }
             },
@@ -101,13 +83,43 @@ export class DesignComponent implements OnInit {
         this.oDesign.sId = base.id;
         this.oDesign.sImageFront = base.image.front;
         this.oDesign.sImageBack = base.image.back;
+        this.setFace(this.oDesign.sFace);
     }
 
     public setFace(face) {
         this.oDesign.sFace = face;
+        if (this.oDesign.sFace === 'front') {
+            this.productImg.load(this.oDesign.sImageFront);
+        } else {
+            this.productImg.load(this.oDesign.sImageBack);
+        }
     }
 
     public setColor(sColor) {
-        this.oDesign.sColor = sColor;
+        this.productColor.fill(sColor);
+    }
+
+    public addImg() {
+        const image = this.draw.image('https://cdn.img42.com/4b6f5e63ac50c95fe147052d8a4db676.jpeg');
+         image.size(100, 100);
+         image.opacity(0.5);
+         image.click(function () {
+         this.selectize();
+         image.resize();
+         image.draggable({
+         minX: 0
+         , minY: 0
+         , maxX: 500
+         , maxY: 530
+         });
+         });
+
+         this.draw.click(function () {
+         // image.selectize(false);
+         });
+
+        // const box = draw.viewbox();
+        // const box = draw.viewbox({ x: 0, y: 0, width: 297, height: 210 });
+        // const zoom = box.zoom;
     }
 }
