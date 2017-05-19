945e4a204521be9e7cef3b48755f6fa8813f0c7b
Nguyen Hung Cuong <hungcuongqn86@gmail.com>
2017-05-19 16:21:55 +0700

    design

--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -18,15 +18,20 @@ export class DesignComponent implements OnInit {
     draw: any;
     productColor: any;
     productImg: any;
+    printable: any;
+    printableConf: any;
+    width: number;
+    height: number;
 
     constructor(private DesignService: DesignService) {
     }
 
     ngOnInit() {
         this.getBaseTypes();
-        this.draw = SVG('drawing').size(530, 630);
-        this.productColor = this.draw.rect(530, 630).fill('#ffffff');
+        this.draw = SVG('drawing');
+        this.productColor = this.draw.rect().fill('#fff');
         this.productImg = this.draw.image();
+        this.printable = this.draw.polyline().fill('none').stroke({width: 1});
     }
 
     private getBaseTypes() {
@@ -57,10 +62,8 @@ export class DesignComponent implements OnInit {
             data => {
                 this.arrBase = data;
                 if (this.arrBase.length > 0) {
-                    // if (this.oDesign.sId === '') {
                     const baseid = this.arrBase[0].id;
                     this.selectBase(baseid);
-                    // }
                 }
             },
             error => {
@@ -83,7 +86,18 @@ export class DesignComponent implements OnInit {
         this.oDesign.sId = base.id;
         this.oDesign.sImageFront = base.image.front;
         this.oDesign.sImageBack = base.image.back;
+        this.printableConf = base.printable;
+        this.width = base.image.width;
+        this.height = base.image.height;
+
+        this.setSize();
         this.setFace(this.oDesign.sFace);
+        this.setPrintable();
+    }
+
+    private setSize() {
+        this.draw.size(this.width, this.height);
+        this.productColor.size(this.width, this.height);
     }
 
     public setFace(face) {
@@ -93,6 +107,31 @@ export class DesignComponent implements OnInit {
         } else {
             this.productImg.load(this.oDesign.sImageBack);
         }
+        this.setPrintable();
+    }
+
+    private setPrintable() {
+        this.printable.clear();
+        const arrPoint: any = [];
+        if (this.oDesign.sFace === 'front') {
+            this.printable.plot([[this.printableConf.front_left, this.printableConf.front_top],
+                [this.printableConf.front_left, Number(this.printableConf.front_top) + Number(this.printableConf.front_height)],
+                [Number(this.printableConf.front_left) + Number(this.printableConf.front_width),
+                    Number(this.printableConf.front_top) + Number(this.printableConf.front_height)],
+                [Number(this.printableConf.front_left) + Number(this.printableConf.front_width)
+                    , Number(this.printableConf.front_top)],
+                [this.printableConf.front_left, this.printableConf.front_top]
+            ]);
+        } else {
+            this.printable.plot([[this.printableConf.back_left, this.printableConf.back_top],
+                [this.printableConf.back_left, Number(this.printableConf.back_top) + Number(this.printableConf.back_height)],
+                [Number(this.printableConf.back_left) + Number(this.printableConf.back_width),
+                    Number(this.printableConf.back_top) + Number(this.printableConf.back_height)],
+                [Number(this.printableConf.back_left) + Number(this.printableConf.back_width)
+                    , Number(this.printableConf.back_top)],
+                [this.printableConf.back_left, this.printableConf.back_top]
+            ]);
+        }
     }
 
     public setColor(sColor) {
@@ -100,23 +139,23 @@ export class DesignComponent implements OnInit {
     }
 
     public addImg() {
-        const image = this.draw.image('https://cdn.img42.com/4b6f5e63ac50c95fe147052d8a4db676.jpeg');
-         image.size(100, 100);
-         image.opacity(0.5);
-         image.click(function () {
-         this.selectize();
-         image.resize();
-         image.draggable({
-         minX: 0
-         , minY: 0
-         , maxX: 500
-         , maxY: 530
-         });
-         });
+        const image = this.draw.image('https://d1b2zzpxewkr9z.cloudfront.net/vectors/Animals%2FSafari%2FElephant%202.svg');
+        image.size(100, 100);
+        image.opacity(0.5);
+        image.click(function () {
+            this.selectize();
+            image.resize();
+            image.draggable({
+                minX: 0
+                , minY: 0
+                , maxX: 500
+                , maxY: 530
+            });
+        });
 
-         this.draw.click(function () {
-         // image.selectize(false);
-         });
+        this.draw.click(function () {
+            // image.selectize(false);
+        });
 
         // const box = draw.viewbox();
         // const box = draw.viewbox({ x: 0, y: 0, width: 297, height: 210 });
