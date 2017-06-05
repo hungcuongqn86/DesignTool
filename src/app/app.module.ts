7a1319b9fcebcd534bc636d5752f9a7cd84f2474
Nguyen Hung Cuong <hungcuongqn86@gmail.com>
2017-06-05 15:41:54 +0700

    launching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {Design, Product, Campaign, DesignService} from './design.service';
 
 import {Ng2UploaderModule} from 'ng2-uploader';
 import {TranslateModule, TranslateLoader, TranslateStaticLoader} from 'ng2-translate/ng2-translate';
+import {FroalaEditorModule, FroalaViewModule} from 'angular2-froala-wysiwyg';
 import {BootstrapModalModule} from 'ng2-bootstrap-modal';
 import {ProductComponent} from './design/product.component';
 import {ColorComponent} from './design/color.component';
@@ -32,7 +33,7 @@ export function createTranslateLoader(http: Http) {
         PricingComponent,
         LaunchingComponent,
         ProductComponent,
-        ColorComponent
+        ColorComponent,
     ],
     imports: [
         BrowserModule,
@@ -45,6 +46,7 @@ export function createTranslateLoader(http: Http) {
             deps: [Http]
         }),
         BootstrapModalModule,
+        FroalaEditorModule.forRoot(), FroalaViewModule.forRoot(),
         routing
     ],
     providers: [
