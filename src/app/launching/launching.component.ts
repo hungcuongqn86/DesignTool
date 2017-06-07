e30034cb80398bc9b14e08026c446aaefd60ec82
Nguyen Hung Cuong <hungcuongqn86@gmail.com>
2017-06-07 16:11:39 +0700

    launching

--- src/app/launching/launching.component.ts
+++ src/app/launching/launching.component.ts
@@ -1,12 +1,18 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {Campaign, DesignService} from '../design.service';
+import {Campaign, Product, DesignService} from '../design.service';
 import {Select2OptionData} from 'ng2-select2';
 import {DialogService} from 'ng2-bootstrap-modal';
 import {ProductdfComponent} from './productdf.component';
 import {Cookie} from 'ng2-cookies';
 import {Observable} from 'rxjs/Rx';
 
+declare const SVG: any;
+const colors: any = {
+    white: {
+        value: '#ffffff'
+    }
+};
 const imgDir = 'http://cdn.30usd.com/images/';
 const campaignCookie = 'campaign_id';
 const userid = 'cuongnh';
@@ -17,6 +23,7 @@ const userid = 'cuongnh';
     styleUrls: ['./launching.component.css']
 })
 export class LaunchingComponent implements OnInit {
+    Product: Product;
     quillOption = {
         toolbar: [
             ['bold', 'italic', 'underline', 'strike'],
@@ -34,9 +41,18 @@ export class LaunchingComponent implements OnInit {
     uri: any = JSON.parse('{"url":""}');
     url: string;
 
+    draw: any;
+    productColor: any;
+    productImg: any;
+    nested: any;
+    arrBase: any = [];
+    face = 'front';
+    color: any = [];
+
     constructor(private router: Router, private DesignService: DesignService,
                 public Campaign: Campaign, private dialogService: DialogService) {
         this.Campaign.step = 3;
+        this.Product = new Product();
 
         this.Campaign.id = 'z8YcVNt1mvGeFbDK';
         if (Cookie.check(campaignCookie)) {
@@ -48,6 +64,11 @@ export class LaunchingComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.draw = SVG('drawingLaunching');
+        this.productColor = this.draw.rect().fill(colors.white.value);
+        this.productImg = this.draw.image();
+        this.nested = this.draw.nested();
+
         this.getDomains();
         this.options = {
             multiple: true
@@ -60,10 +81,23 @@ export class LaunchingComponent implements OnInit {
                 Object.keys(res).map((index) => {
                     this.Campaign[index] = res[index];
                 });
+                if (this.Campaign.products.length) {
+                    const checkExit = this.Campaign.products.findIndex(x => x.default === true);
+                    if (checkExit >= 0) {
+                        Object.keys(this.Campaign.products[checkExit]).map((index) => {
+                            this.Product[index] = this.Campaign.products[checkExit][index];
+                        });
+                    }
+                }
                 const checkS = this.Campaign.url.slice(-1);
                 if (checkS !== '/') {
                     this.url = this.Campaign.url + '/';
                 }
+                if (this.Product.back_view) {
+                    this.face = 'back';
+                }
+                this.color = this.getColor(this.color, this.Product.colors);
+                this.getBases();
                 this.getCategories();
             },
             error => {
@@ -75,6 +109,24 @@ export class LaunchingComponent implements OnInit {
 
     public getBaseImgUrl(sFace, base: any) {
         return imgDir + base + '_' + sFace + '.png';
+    }
+
+    private getColor(color: any, arrcolors: any) {
+        if (arrcolors.length) {
+            const check = arrcolors.findIndex(x => x.id === color.id);
+            if (check < 0) {
+                const index = arrcolors.findIndex(x => x.default === true);
+                if (index < 0) {
+                    return arrcolors[0];
+                } else {
+                    return arrcolors[index];
+                }
+            } else {
+                return arrcolors[check];
+            }
+        } else {
+            return null;
+        }
     }
 
     public setVisibility(val) {
@@ -176,10 +228,74 @@ export class LaunchingComponent implements OnInit {
                     this.Campaign.products[index].default = true;
                     this.Campaign.products[index].back_view = product.back_view;
                     this.Campaign.products[index].colors = product.colors;
+                    Object.keys(this.Campaign.products[index]).map((key) => {
+                        this.Product[key] = this.Campaign.products[index][key];
+                    });
                 } else {
                     this.Campaign.products[index].default = false;
                 }
             });
+            if (this.Product.back_view) {
+                this.face = 'back';
+            } else {
+                this.face = 'front';
+            }
+            this.color = this.getColor(this.color, this.Product.colors);
+            this.getBases();
+        }
+    }
+
+    private getBases() {
+        this.DesignService.getBases(this.Product.base.type_id).subscribe(
+            data => {
+                this.arrBase = data;
+                this.selectBase(this.Product.base.id);
+            },
+            error => {
+                console.error(error.json().message);
+                return Observable.throw(error);
+            }
+        );
+    }
+
+    private selectBase(id) {
+        for (let i = 0; i < this.arrBase.length; i++) {
+            const value = this.arrBase[i].id;
+            if (value === id) {
+                this._selectBase(this.arrBase[i]);
+            }
+        }
+    }
+
+    private _selectBase(base: any) {
+        this.Product.base = base;
+        this.setView();
+    }
+
+    public setView() {
+        const myjs = this;
+        const maxH = 500;
+        const maxW = 400;
+        this.productImg.load(this.getBaseImgUrl(this.face, this.Product.base.id)).loaded(function (loader) {
+            const tl: number = Number((loader.width / loader.height).toFixed(2));
+            let rsW: number = loader.width;
+            let rsH: number = loader.height;
+            if (rsH > maxH) {
+                rsH = maxH;
+                rsW = Number((rsH * tl).toFixed(2));
+                if (rsW > maxW) {
+                    rsW = maxW;
+                    rsH = Number((rsW / tl).toFixed(2));
+                }
+            }
+            myjs.draw.size(rsW, rsH);
+            myjs.productImg.size(rsW, rsH);
+            myjs.productColor.size(rsW, rsH);
+            const zoom = Number((rsW / myjs.Product.base.image.width).toFixed(2));
+            // myjs.genDesign(zoom);
+        });
+        if (this.color) {
+            this.productColor.fill(this.color.value);
         }
     }
 }
